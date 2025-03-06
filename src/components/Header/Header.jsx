b90fec41f339a394481f0712f2496c4a53d280ea
Phongsathon Markdankhang <poplower66@gmail.com>
2025-03-06 19:31:43 +0700

    resume download

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,8 @@ const Header = () => {
         <div className="header-content">
           <h1>Phongsathon Makdankang</h1>
           <p>👨‍💻An enthusiastic third-year student at the College of Social Communication Innovation, majoring in Computer for Communication. Passionate about web development with React.js. Seeking opportunities to apply my skills and knowledge in an internship position to further prepare for my future career in this field.</p>
-          <a href="#" className="download-button">
+          <a href="https://drive.google.com/file/d/11DSN4A2kZOLxedYDda9DJS3q0gZ1ru2C/view?usp=sharing" target="_blank" 
+                 rel="noopener noreferrer" className="download-button">
             Download Resume
           </a>
         </div>
